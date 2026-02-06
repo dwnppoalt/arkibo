104d8d07f5871cfde1e4876771a67cd05a139328
dwnppoalt <dwnppoalt@outlook.com>
2026-02-06 21:16:58 +0800

    add(font): add Montserrat and Readex Pro as fonts, changing home.fxml to Readex Pro to demonstrate such effect

--- src/main/java/org/arkibo/controllers/HomeController.java
+++ src/main/java/org/arkibo/controllers/HomeController.java
@@ -1,4 +1,18 @@
 package org.arkibo.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
 public class HomeController {
+
+    @FXML
+    private VBox root;
+
+    @FXML
+    public void initialize() {
+        root.getStylesheets().add(
+            getClass().getResource("/css/home.css").toExternalForm()
+        );
+    }
+
 }
