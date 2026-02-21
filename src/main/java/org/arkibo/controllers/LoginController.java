273cae0c5bfa16642b55d03c202ab7fa1a98f52f
dwnppoalt <dwnppoalt@outlook.com>
2026-02-21 11:37:42 +0800

    fix(login): fixed a bug in login where multiple attempts to login with Google raises an error

--- src/main/java/org/arkibo/controllers/LoginController.java
+++ src/main/java/org/arkibo/controllers/LoginController.java
@@ -1,5 +1,7 @@
 package org.arkibo.controllers;
 
+import java.util.concurrent.TimeoutException;
+
 import org.arkibo.app.auth.AuthService;
 import org.arkibo.app.state.AppState;
 import org.arkibo.app.state.StatefulController;
@@ -7,6 +9,7 @@ import org.arkibo.models.User.User;
 import org.arkibo.router.Router;
 import org.arkibo.utils.Logger;
 
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.Button;
@@ -35,32 +38,42 @@ public class LoginController implements StatefulController {
         Logger.log("LOGIN-CONTROLLER", "initialize called");
         login.getStylesheets().add(
                 getClass().getResource("/css/login.css").toExternalForm());
-        
-        googleLogin.setOnMouseClicked(e -> Logger.log("LOGIN", "Mouse clicked"));
 
     }
 
     @FXML
     private void googleButtonOnClick() {
-        Logger.log("LOGIN-CONTROLLER", "Logging in...");
-            new Thread(() -> {
-                try {
-                User user = authService.login();
-                
-                javafx.application.Platform.runLater(() -> {
-                    appState.getUserSession().set(user);
-                    Router.goTo("views/home.fxml");
-                });
+        Logger.log("LOGIN-CONTROLLER", "Logging in with Google...");
 
-            } catch (Exception e) {
-                e.printStackTrace();
+        Task<User> loginTask = new Task<>() {
+            @Override
+            protected User call() throws Exception {
+                return authService.login();
             }
-        }).start();
+        };
+
+        loginTask.setOnSucceeded(event -> {
+            User user = loginTask.getValue();
+            appState.getUserSession().set(user);
+            Router.goTo("views/home.fxml");
+        });
+
+        loginTask.setOnFailed(event -> {
+            Throwable ex = loginTask.getException();
+            if (ex instanceof TimeoutException) {
+                Logger.log("LOGIN", "Login with Google timeout.");
+            } else {
+                ex.printStackTrace();
+            }
+        });
+
+        new Thread(loginTask).start();
     }
     
 
     @FXML
     private void guestButtonOnClick() {
+        Logger.log("LOGIN", "Logging in as guest...");
         User user = new User("-1", "Guest User", null, null, null);
         appState.getUserSession().set(user);
         Router.goTo("views/home.fxml");
